f2d4fa8d5a28c7aebf1932dffe4ebd38d200580e
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2024-10-15 12:51:49 +0900

    chapter6

--- chap5-01/src/components/Emotion.jsx
+++
@@ -1,4 +0,0 @@
-/** コンポーネントファイルの中に CSS を書く方法 */
-// /** @jsxImportSource @emotion/react */
-// import { jsx, css } from "@emotion/react";
-import styled from "@emotion/styled";
--- chap6-01/src/index.js
+++ chap6-01/src/index.js
@@ -1,7 +1,5 @@
 import ReactDOM from "react-dom/client";
-// import {Fragment} from "react";
 import { App } from "./App";
-// import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />)
