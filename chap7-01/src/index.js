39bf3c897fbaa372527ad5a7827c8d500c31cfb4
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2024-10-22 13:58:59 +0900

    一旦コミットするけど・・

--- chap7-01/src/index.js
+++ chap7-01/src/index.js
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import { AdminFlagProvider } from "./components/providers/AdminFlagProvider";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />)
+root.render(
+    <AdminFlagProvider>
+        <App />
+    </AdminFlagProvider>
+)
