6b5aa66365dedbe494426253c8de71dc9292d48e
Hiroyuki Sasaki <sasaki@yoshida-g.ac.jp>
2024-11-12 15:10:29 +0900

    chap9教材

--- chap8-02/src/components/ListItem.jsx
+++
@@ -1,8 +0,0 @@
-export const ListItem = (props) => {
-  const { id, name, age, personalColor = "grey", hobbies } = props;
-  return (
-    <p style={{ color: personalColor }}>
-      {id}:{name}({age}) {hobbies?.join(" / ")}
-    </p>
-  );
-};
